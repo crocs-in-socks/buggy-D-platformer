c93791eaee038f9e6fa5fcb09d6c17c6b082cf3c
crocs-in-socks <gouriswag1@gmail.com>
2022-10-02 05:09:33 +0530

    Backgrounds

---
+++ buggy-d-platformer/Assets/AudioManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    void Start()
+    {
+        Play("Game_Theme");
+    }
+
+    public void Play(string name)
+    {
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+       s.source.Play();
+    }
+}
